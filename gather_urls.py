216974323df51324128bc70a83dc1c4b3294c680
Benjamin van der Burgh <benjamin.vanderburgh@incentro.com>
2019-02-02 11:27:34 +0100

    Added scripts

---
+++ gather_urls.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+
+import click
+import requests
+from bs4 import BeautifulSoup
+
+
+TEMPLATE_URL = 'https://www.hebban.nl/main/Review/more?offset={}&step={}'
+
+
+@click.command()
+@click.argument('outfile')
+@click.option('--offset', default=0, help='Review offset.')
+@click.option('--step', default=1000, help='Number of review urls to fetch per request.')
+def gather(outfile, offset, step):
+    """
+    This script gathers review urls from Hebban and writes them to OUTFILE.
+    """
+    urls = []
+    while True:
+        target_url = TEMPLATE_URL.format(offset, step)
+        r = requests.get(target_url)
+        data = r.json()
+
+        if not data['html']:
+            break
+
+        soup = BeautifulSoup(data['html'], 'lxml')
+        new_urls = [div['data-url'] for div in soup('div', {'class': 'item'})]
+        print("Fetched {} urls from {}".format(len(new_urls), target_url))
+        urls.extend(new_urls)
+        offset += 1000
+
+    with open(outfile, 'w') as f:
+        for url in urls:
+            f.write(url)
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    gather()
---
+++ scrape_reviews.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python2
+
+import time
+import codecs
+import json
+
+import click
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from progressbar import ProgressBar
+
+
+# NOTE: this is often needed when a page is still loading
+def retry(fun, arg, max_retries=10, sleep=0.5):
+    """
+    Retry executing function FUN with arguments ARG.
+    :param fun: function to execute
+    :param arg: argument to pass to FUN
+    :param max_retries: maximum number of retries
+    :param sleep: number of seconds to sleep in between retries
+    :return:
+    """
+    data = fun(arg)
+    retries = 0
+    while not data and retries < max_retries:
+        time.sleep(sleep)
+        data = fun(arg)
+        retries = retries + 1
+    return data if data else []
+
+
+@click.command()
+@click.argument('infile')
+@click.argument('outfile')
+@click.option('--encoding', default='utf-8', help='Output file encoding.')
+@click.option('--indent', default=2, help='Output JSON file with scraped data.')
+def scrape(infile, outfile, encoding, indent):
+    """
+    Iterate over review urls in INFILE text file, scrape review data and output to OUTFILE.
+    """
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
+
+    urls = [line.strip() for line in open(infile)]
+
+    reviews = []
+    bar = ProgressBar()
+    errors = []
+    for url in bar(urls):
+        try:
+            driver.get(url)
+            title = retry(driver.find_elements_by_css_selector, "div[itemprop='itemReviewed']")
+            author = retry(driver.find_elements_by_css_selector, "a[class='author']")
+            reviewer = retry(driver.find_elements_by_class_name, 'user-excerpt-name')
+            rating = retry(driver.find_elements_by_css_selector, '.fa-star.full')
+            text = retry(driver.find_elements_by_xpath, '//../following-sibling::p')
+            published = retry(driver.find_elements_by_css_selector, "meta[itemprop='datePublished'")
+
+            if text and rating:
+                text = '\n'.join([p.text.strip() for p in text]).strip()
+                if text:
+                    reviews.append({
+                        'url': url,
+                        'title': title[0].get_attribute('data-url').strip() if title else None,
+                        'author': author[0].get_attribute('href').strip() if author else None,
+                        'reviewer': reviewer[0].get_attribute('href').strip() if reviewer else None,
+                        'rating': len(rating),
+                        'text': text,
+                        'published': published[0].get_attribute('content').strip() if published else None
+                    })
+        except Exception:
+            errors.append(url)
+            print("Error {}: {}".format(len(errors), url))
+            continue
+
+    print("Finished scraping {} urls with {} errors.".format(len(urls), len(errors)))
+
+    print("Writing reviews to {}".format(outfile))
+    with codecs.open(outfile, 'w', encoding=encoding) as f:
+        json.dump(reviews, f, ensure_ascii=False, indent=indent)
+
+
+if __name__ == '__main__':
+    scrape()
